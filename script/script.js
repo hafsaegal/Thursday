303ee46e7c6ad23a524b7309fa06f878d7ff62c3
Hafsa Egal <hafsaegal915@gmail.com>
2025-01-29 15:37:59 +0300

    wednesday week4 c/w

--- script/script.js
+++ script/script.js
@@ -1,45 +1,16 @@
-// const book = {
-//   title: "Jack and the bean stalk",
-//   author: { name: "JosephJack" },
-//   pages: 20,
-//   isPublished: true,
-// };
-// console.log(book.title);
-// console.log(book.author.name);
-// console.log("Title:", book.title);
-
-// const colors = ["red", "yellow", "purple", "blue", "green"];
-// console.log(colors);
-
-function sayHello() {
-  console.log("Hello");
-}
-sayHello();
-
-function world() {
-  console.log("world");
-}
-world();
-
-//function expression/we call the variable name
-let hello = function () {
-  console.log("hello");
-};
-hello();
-
-//Arrow function
-// let greeting = () => {
-//   console.log("Hello1");
-
-// Hello1();
-
-let add = (x, y) => {
-  return x + y;
-};
-console.log(add(3, 6));
-
-// Anonymous function
-
-hello(function () {
-  console.log("Anonymous");
-});
+//
+const fruits = ["Apples", "Mangoes", "Oranges", "Bananas", "strawberries"];
+console.log(fruits)
+fruits.push("Guava")
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
+fruits.shift()
+console.log(fruits)
+fruits.unshift('Apple')
+console.log(fruits)
+const deleted= fruits.slice(2,1)
+console.log(deleted);
+console.log(fruits);
+fruits.splice(1,1)
+console.log(fruits)
