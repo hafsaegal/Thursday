9cf88e1df51ce2aea551fd59d4816c2a2cff00aa
Hafsa Egal <hafsaegal915@gmail.com>
2025-02-01 06:01:38 +0300

    saving work

--- script/script.js
+++ script/script.js
@@ -14,3 +14,19 @@ console.log(deleted);
 console.log(fruits);
 fruits.splice(1,1)
 console.log(fruits)
+
+//iteration methods
+
+const nums= [1,2,3,4,5]
+
+//for loop
+for (let i =0, i<nums.length; i++) { 
+  const element = nums [i];...0,1,2,3,4,5
+  console.log(element)...c 1,2,3,4,5
+}
+
+let count 0;
+
+while (count<5){
+  console.log
+}
